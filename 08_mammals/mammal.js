43c081e9569b4851750f489e36bbfe040bf433e2
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2016-02-12 15:51:39 -0500

    finished 8/8

---
+++ 08_mammals/mammal.js
@@ -0,0 +1,30 @@
+var Mammal = function(name){
+  this.name = name;
+  this.offspring = [];
+}
+
+Mammal.prototype.sayHello = function(){
+  return "My name is " + this.name + ", I'm a Mammal";
+}
+
+Mammal.prototype.haveBaby = function(){
+  var child = new this.constructor("Baby " + this.name);
+  this.offspring.push(child);
+  return child;
+}
+
+var Cat = function(name, color){
+  // this.name = name;
+  // this.offspring = [];
+  Mammal.call(this, name);
+  this.color = color;
+}
+
+Cat.prototype = new Mammal;
+Cat.prototype.constructor = Cat;
+
+Cat.prototype.haveBaby = function(color){
+  var kitten = Mammal.prototype.haveBaby.call(this);
+  kitten.color = color;
+  return kitten;
+}
